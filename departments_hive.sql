beaa9c19f315ae4adc5978e48c9caefcf32cdcf7
Jeydata2019 <57989145+Jeydata2019@users.noreply.github.com>
2024-06-01 05:41:12 -0400

    Update departments_hive.sql

--- departments_hive.sql
+++ departments_hive.sql
@@ -9,8 +9,8 @@ USE hr_db;
 
 --Creates employees table
 CREATE TABLE IF NOT EXISTS departments (
-    department_name STRING,
-    department_id INT
+    department_id INT,
+    department_name STRING
 )
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY ','
